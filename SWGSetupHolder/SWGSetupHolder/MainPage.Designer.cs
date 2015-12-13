faf551fe18c9a4ee41a3d16d582bcee516304bf6
Iosnowore <pressinger.gene66@gmail.com>
2015-12-13 12:12:40 -0500

    Added interface buttons

--- SWGSetupHolder/SWGSetupHolder/MainPage.Designer.cs
+++ SWGSetupHolder/SWGSetupHolder/MainPage.Designer.cs
@@ -28,23 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.MakeNewSetupButton = new System.Windows.Forms.Button();
+            this.ViewSavedSetupsButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // Form1
+            // MakeNewSetupButton
+            // 
+            this.MakeNewSetupButton.Location = new System.Drawing.Point(79, 32);
+            this.MakeNewSetupButton.Name = "MakeNewSetupButton";
+            this.MakeNewSetupButton.Size = new System.Drawing.Size(120, 30);
+            this.MakeNewSetupButton.TabIndex = 0;
+            this.MakeNewSetupButton.Text = "Make New Setup";
+            this.MakeNewSetupButton.UseVisualStyleBackColor = true;
+            // 
+            // ViewSavedSetupsButton
+            // 
+            this.ViewSavedSetupsButton.Location = new System.Drawing.Point(79, 193);
+            this.ViewSavedSetupsButton.Name = "ViewSavedSetupsButton";
+            this.ViewSavedSetupsButton.Size = new System.Drawing.Size(120, 30);
+            this.ViewSavedSetupsButton.TabIndex = 1;
+            this.ViewSavedSetupsButton.Text = "View Saved Setups";
+            this.ViewSavedSetupsButton.UseVisualStyleBackColor = true;
+            // 
+            // MainPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.ViewSavedSetupsButton);
+            this.Controls.Add(this.MakeNewSetupButton);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
-            this.Name = "Form1";
+            this.Name = "MainPage";
             this.Text = "Setup Holder";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button MakeNewSetupButton;
+        private System.Windows.Forms.Button ViewSavedSetupsButton;
     }
 }
 
