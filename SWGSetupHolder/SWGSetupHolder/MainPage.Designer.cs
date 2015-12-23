c63699d82450b6f534f2af99cbfb9ff674b37246
Iosnowore <pressinger.gene66@gmail.com>
2015-12-22 23:57:04 -0500

    Minor rename

--- SWGSetupHolder/SWGSetupHolder/MainPage.Designer.cs
+++ SWGSetupHolder/SWGSetupHolder/MainPage.Designer.cs
@@ -106,7 +106,7 @@
             this.MaximizeBox = false;
             this.Name = "MainPage";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "SWG Setup Organizer";
+            this.Text = "Trooper Setup Organizer";
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- SWGSetupHolder/SWGSetupHolder/Properties/AssemblyInfo.cs
+++ SWGSetupHolder/SWGSetupHolder/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SWG Setup Organizer")]
+[assembly: AssemblyTitle("Trooper Setup Organizer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SWG Setup Organizer")]
+[assembly: AssemblyProduct("Trooper Setup Organizer")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
